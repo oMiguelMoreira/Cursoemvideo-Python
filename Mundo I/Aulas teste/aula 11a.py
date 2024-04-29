328d80b2f48d211a7295512d01d830230823476a
oMiguelMoreira <mm6867565@gmail.com>
2024-04-29 12:19:40 -0300

    resolução de exercícios

---
+++ Mundo I/Aulas teste/aula 11a.py
@@ -0,0 +1,6 @@
+nome = 'Miguel'
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome,'\033[m' ))
+
+'''a=5
+b=6
+print('O valores são \033[32m{}\033[m e \033[31m{}\033[m!!!'.format(a,b))'''
